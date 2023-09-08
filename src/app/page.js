94d3d76f28d74b966dacd798c36b0090bc6f497c
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2023-09-08 20:55:03 -0300

    Avances

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,32 @@
+'use client'
 import React from 'react';
 import '../../public/Main.scss';
 
 const PortfolioPage = () => {
 
-  
-  return (
-   
-    
-<div className="wrapper">
-  <div className="one">One</div>
-  <div className="two">Two</div>
-  <div className="three">Three</div>
-  <div className="four">Four</div>
-  <div className="five">Five</div>
-  <div className="six">Six</div>
-  <div className="seven">Seven</div>
-  <div className="eight">Eight</div>
-  <div className="nine">Nine</div>
-  <div className="ten">Ten</div>
-  <div className="eleven">Eleven</div>
-</div>
+  const handleButtonClick = (buttonNumber) => {
 
+    alert(`Clic en el botón ${buttonNumber}`);
+  };
+  return (
+    <> 
+    <div className='Presentacion'>
+      <h1 className='bienvenida'>Bienvenido a mi portafolio Online</h1>
+    </div>
+    <div className="wrapper">
+      <div className="one" onClick={() => handleButtonClick(1)}>One</div>
+      <div className="two" onClick={() => handleButtonClick(2)}>Two</div>
+      <div className="three" onClick={() => handleButtonClick(3)}>Three</div>
+      <div className="four" onClick={() => handleButtonClick(4)}>Four</div>
+      <div className="five" onClick={() => handleButtonClick(5)}>Five</div>
+      <div className="six" onClick={() => handleButtonClick(6)}>Six</div>
+      <div className="seven" onClick={() => handleButtonClick(7)}>Seven</div>
+      <div className="eight" onClick={() => handleButtonClick(8)}>Eight</div>
+      <div className="nine" onClick={() => handleButtonClick(9)}>Nine</div>
+      <div className="ten" onClick={() => handleButtonClick(10)}>Ten</div>
+      <div className="eleven" onClick={() => handleButtonClick(11)}>Eleven</div>
+    </div>
+    </>
   );
 };
 
