c08861be3d3de7a3767c7a00adebef77f95b9b09
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2023-09-09 16:16:45 -0300

    Casi termiando
    Faltan modificaciones en la funcionalidad de la aplicacion como redirecion de botones y modificar iconos o divs

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,44 @@
 'use client'
-import React from 'react';
+import React , {useState , useEffect}from 'react';
 import '../../public/Main.scss';
 
 const PortfolioPage = () => {
 
-  const handleButtonClick = (buttonNumber) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    alert(`Clic en el botón ${buttonNumber}`);
+  const handleButtonClick = () => {
+    setIsModalOpen(true);
   };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  
   return (
     <> 
-    <div className='Presentacion'>
-      <h1 className='bienvenida'>Bienvenido a mi portafolio Online</h1>
-    </div>
     <div className="wrapper">
-      <div className="one" onClick={() => handleButtonClick(1)}>One</div>
-      <div className="two" onClick={() => handleButtonClick(2)}>Two</div>
-      <div className="three" onClick={() => handleButtonClick(3)}>Three</div>
-      <div className="four" onClick={() => handleButtonClick(4)}>Four</div>
-      <div className="five" onClick={() => handleButtonClick(5)}>Five</div>
-      <div className="six" onClick={() => handleButtonClick(6)}>Six</div>
-      <div className="seven" onClick={() => handleButtonClick(7)}>Seven</div>
-      <div className="eight" onClick={() => handleButtonClick(8)}>Eight</div>
-      <div className="nine" onClick={() => handleButtonClick(9)}>Nine</div>
-      <div className="ten" onClick={() => handleButtonClick(10)}>Ten</div>
-      <div className="eleven" onClick={() => handleButtonClick(11)}>Eleven</div>
+      <div className="one" onClick={() => handleButtonClick()}><div className="overlay">Presiona aqui para saber mas acerca de mi</div></div>
+      <div className="two" onClick={() => handleButtonClick()}><div className="overlay">Texto relacionado a Two</div></div>
+      <div className="three" onClick={() => handleButtonClick()}></div>
+      <div className="four" onClick={() => handleButtonClick()}>  <div className="overlay">Four</div></div>
+      <div className="five" onClick={() => handleButtonClick()}>  <div className="overlay">Tienda de Diseños</div></div>
+      <div className="six" onClick={() => handleButtonClick()}>  <div className="overlay">Six</div></div>
+      <div className="seven" onClick={() => handleButtonClick()}>  <div className="overlay">Seven</div></div>
+      <div className="eight" onClick={() => handleButtonClick()}>  <div className="overlay">Eight</div></div>
+      <div className="nine" onClick={() => handleButtonClick()}>  <div className="overlay">Nine</div></div>
+      <div className="ten" onClick={() => handleButtonClick()}>  <div className="overlay">Proyectos</div></div>
+      <div className="eleven" onClick={() => handleButtonClick()}> <div className="overlay">Eleven</div></div>
     </div>
+    {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            
+            <h2>Información del botón</h2>
+            <p>Este es el contenido del modal.</p>
+            <button onClick={closeModal}>Cerrar</button>
+          </div>
+        </div>
+      )}
     </>
   );
 };
