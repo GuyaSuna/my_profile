ec83d9edf1af471152384677a96a072a800e87a0
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2023-09-11 17:34:32 -0300

    yeah

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 'use client'
 import React , {useState , useEffect}from 'react';
 import '../../public/Main.scss';
+import  WidgetBot ,  {  API  }  from  '@widgetbot/react-embed'
 
 const PortfolioPage = () => {
 
-  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleButtonClick = () => {
     setIsModalOpen(true);
@@ -13,41 +14,41 @@ const PortfolioPage = () => {
   const closeModal = () => {
     setIsModalOpen(false);
   };
-
-  useEffect (() =>{
-    const timer = setTimeout(() => {
+  const handleModalClick = (e) => {
+    if (e.target.classList.contains('modal')) {
       closeModal();
-    }, 5000);
+    }
+  };
 
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
-  
+  const modalInfo = () =>{
+
+  }
+
   return (
     <> 
     <div className="wrapper">
       <div className="one" onClick={() => handleButtonClick()}> <div className="overlay">Presiona aqui para saber mas acerca de mi</div></div>
-      <div className="two" onClick={() => handleButtonClick()}><div className="overlay">Texto relacionado a Two</div></div>
+      <div className="two" onClick={() => handleButtonClick()}><div className="overlay">Mis Redes</div></div>
       <div className="three" onClick={() => handleButtonClick()}></div>
-      <div className="four" onClick={() => handleButtonClick()}> <div className="overlay">Four</div></div>
+      <div className="four" onClick={() => handleButtonClick()}><div className="overlay">Comunicate conmigo</div></div>  
       <div className="five" onClick={() => handleButtonClick()}>  <div className="overlay">Tienda de Diseños</div></div>
       <div className="six" onClick={() => handleButtonClick()}>  <div className="overlay">Six</div></div>
       <div className="seven" onClick={() => handleButtonClick()}>  <div className="overlay">Seven</div></div>
       <div className="eight" onClick={() => handleButtonClick()}>  <div className="overlay">Eight</div></div>
       <div className="nine" onClick={() => handleButtonClick()}>  <div className="overlay">Nine</div></div>
       <div className="ten" onClick={() => handleButtonClick()}>  <div className="overlay">Proyectos</div></div>
-      <div className="eleven" onClick={() => handleButtonClick()}> <div className="overlay">Eleven</div></div>
+      <div className="eleven" onClick={() => handleButtonClick()}> <div className="overlay">Curriculum</div></div>
     </div>
     {isModalOpen && (
-        <div className="modal">
-          <div className="modal-content">
-            
-            <h2>Información del botón</h2>
-            <p>Este es el contenido del modal.</p>
-            <button onClick={closeModal}>Cerrar</button>
-          </div>
-        </div>
+ <div className="modal" onClick={handleModalClick}>
+ <div className="modal-content">
+   <WidgetBot
+     className='ds'
+     server="1150805905982619670"
+     channel="1150805907106705482"
+   />
+ </div>
+</div>
       )}
     </>
   );
