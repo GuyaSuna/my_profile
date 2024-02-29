b89480456a36802576273b20eb692d4a420c4a03
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2024-02-28 21:17:45 -0300

    Modificaciones de estilo
    Falta Proyectos y Conocimientos

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,8 @@
 import React , {useState , useEffect}from 'react';
 import '../../public/Main.scss';
 import  WidgetBot ,  {  API  }  from  '@widgetbot/react-embed';
-import video from '../../public/VideoParaPorfolio.gif'
+
+
 
 
 import { useRouter } from 'next/navigation';
@@ -12,12 +13,37 @@ const PortfolioPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [imagen, setImagen] = useState('Basic');
   const [Id , setId] = useState(0);
-  const [backgroundIndex, setBackgroundIndex] = useState(0);
   const [dataPosition , setDataPosition] = useState(0);
   const [gamer , setGamer] = useState(false);
   const [isTransitioning, setIsTransitioning] = useState(false);
   const [showDiv, setShowDiv] = useState(false);
+  const [selectorValue, setSelectorValue] = useState('opcion1'); 
+  const [inputValue, setInputValue] = useState('');
   const router = useRouter();
+  const [isNormal, setIsNormal] = useState(true);
+  const [nombre, setNombre] = useState('');
+  const [email, setEmail] = useState('');
+  const [telefono, setTelefono] = useState('');
+  const [displayText, setDisplayText] = useState('');
+
+  function toggleAnimation() {
+    setIsNormal(!isNormal);
+  }
+
+  let text = 'Bienvenido a mi mercado, abre la pestaña a tu derecha y rellena la informacion necesaria para que pueda saber que es lo que buscas'
+  useEffect(() => {
+    let currentIndex = 0;
+    const intervalId = setInterval(() => {
+      if (currentIndex <= text.length) {
+        setDisplayText(text.substring(0, currentIndex));
+        currentIndex++;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 50); 
+
+    return () => clearInterval(intervalId);
+  }, [Id==5]);
 
 
   const cambioInfo = (data) => {
@@ -28,17 +54,16 @@ const PortfolioPage = () => {
 
   let fondos = [
     "",
-    "VideoParaPorfolio.gif",
+    "https://i.pinimg.com/originals/66/19/80/661980c1f6ed5448d6825d5920048bbb.gif",
     "Redes.jpg",
     "",
     "https://wallpaperaccess.com/full/8351153.gif",
-    "https://i.pinimg.com/564x/92/ba/af/92baafd195340f7a8d9c0c7495deda05.jpg",
+    "",
     "",
     "https://w0.peakpx.com/wallpaper/701/1001/HD-wallpaper-among-us-minimalist-black-background-among-us.jpg",
-    "https://i.pinimg.com/originals/8d/66/9b/8d669b63358117e3ff9fb28f1d7bb3c7.jpg",
-    "",
-    "https://64.media.tumblr.com/cb33ede8c01a3006989f79902e144e7d/801da6a4894a4104-ae/s640x960/291a0ae9b07a8d181c43d06cd6030b9f4e2ea662.gifv",
-    "https://i.pinimg.com/564x/71/66/84/716684860515165ac058200342f3d789.jpg"
+    "https://i.pinimg.com/564x/ec/44/64/ec44647bcbe23e5f13cc605bc2e57d79.jpg",
+    "https://i.pinimg.com/564x/9d/e6/cd/9de6cddc05ea745de25b1d77b476efb2.jpg",
+   
   ]
 
   let fondosGamer = [
@@ -50,10 +75,9 @@ const PortfolioPage = () => {
     "",
     "https://i.pinimg.com/736x/2b/3c/3d/2b3c3d5f7633f8c76c55141efb5b43d7.jpg",
     "https://w0.peakpx.com/wallpaper/701/1001/HD-wallpaper-among-us-minimalist-black-background-among-us.jpg",
-    "https://i.pinimg.com/originals/8d/66/9b/8d669b63358117e3ff9fb28f1d7bb3c7.jpg",
-    "",
-    "https://64.media.tumblr.com/cb33ede8c01a3006989f79902e144e7d/801da6a4894a4104-ae/s640x960/291a0ae9b07a8d181c43d06cd6030b9f4e2ea662.gifv",
-    "https://cdn.dribbble.com/users/439871/screenshots/4269563/cv.jpg"
+    "fondo_div.png",
+    "https://i.pinimg.com/564x/9d/e6/cd/9de6cddc05ea745de25b1d77b476efb2.jpg",
+  
   ]
 
   const cambiarImagen = () => {
@@ -68,6 +92,9 @@ const PortfolioPage = () => {
     }, 2000); 
   };
 
+
+
+
   useEffect(() => {
     if(Id != 0){
     setIsModalOpen(true);
@@ -76,11 +103,11 @@ const PortfolioPage = () => {
 
   const closeModal = () => {
     setIsModalOpen(false);
+    setId(0);
   };
   const handleModalClick = (e) => {
     if (e.target.classList.contains('modal')) {
       closeModal();
-      setId(0);
     }
   };
 
@@ -91,8 +118,21 @@ const PortfolioPage = () => {
     router.push("https://github.com/GuyaSuna")
   }
 
+  const descargarCurriculum = () => {
+    const url = '/curriculum.pdf'; 
+
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'curriculum.pdf');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <> 
+
      {showDiv && (
         <div className={`transitioning-div${isTransitioning ? ' transitioning' : ''}`}>
 
@@ -112,19 +152,21 @@ const PortfolioPage = () => {
       <div className={`four ${imagen}`} onClick={() =>setId(4)}><div className="overlay">Canal de Discord</div></div>  
       <div className={`five ${imagen}`} onClick={() =>setId(5)}>  <div className="overlay">Tienda de Software</div></div>
       <div className={`nine ${imagen}`} onClick={() =>setId(9)}>  <div className="overlay">Proyectos</div></div>
-      <div className={`seven ${imagen}`} onClick={() =>setId(5)}>  <div className="overlay">aca</div></div>
-      <div className={`eight ${imagen}`}  onClick={() =>setId(9)}>  <div className="overlay">Proyectos</div></div>
-      <div className={`ten ${imagen}`} onClick={() =>cambiarImagen()}>  <div className="overlay">Modifica el estilo</div></div>
+      <div className={`seven ${imagen}`} onClick={() =>descargarCurriculum()}>  <div className="overlay">Curriculum</div></div>
+      <div className={`eight ${imagen}`}  onClick={() =>setId(8)}>  <div className="overlay">Conocimientos</div></div>
+      <div className={`ten ${imagen}`} onClick={() =>cambiarImagen()}>  <div className="overlay">Modificar estilo</div></div>
      
      </div>   </div >
   )}
     {isModalOpen && (
   <div className="modal"  onClick={handleModalClick}>
  <div className="modal-content" style={{ backgroundImage: `url(${gamer ? fondosGamer[Id] : fondos[Id]})` }}>
+
     {Id === 1  &&
-
-
-      <div className={`container ${imagen}`}>
+    <>
+  
+      <div className={`container ${imagen}`}> 
+        <div className="close-button-one" onClick={() => closeModal()}></div>
         <div className={`informacion ${imagen}`}>
          { dataPosition != 0 && <div className='anterior' onClick={ () =>cambioInfo(true)}/>} 
        { dataPosition == 0 && <div> <h1 className='infOne'>Hola a todos!</h1>
@@ -152,31 +194,73 @@ const PortfolioPage = () => {
     </div>  </div>}
        {dataPosition != 2 && <div className='siguiente' onClick={() => cambioInfo(false)}/>}
         </div>
-      </div>
+      </div></>
       }
             {Id === 2  &&
+            <div>
+                <div className={`container ${imagen}`}> <div className="close-button" onClick={() => closeModal()}></div> </div>
               <p> two </p>
+              </div>
       }
             {Id === 3  &&
-              <p> three</p>
+              <div>
+              <div className={`container ${imagen}`}> <div className="close-button" onClick={() => closeModal()}></div> </div>
+              <p> ESTO ESTA EN CREACION Y TODAVIA NO FUNCIONA</p>
+              </div>
       }
       {Id === 4  &&
+
+       <div className={`container ${imagen}`}>       <div className="close-button-one" onClick={() => closeModal()}></div>
         <WidgetBot
           className="ds"
           server="1150805905982619670"
           channel="1150805907106705482"
         />
+        </div>
       }
-          {Id === 5  &&
-          <div className={`container2 ${imagen}`}> 
-          <div className='chat-burbuja'> 
-             Bienvenido a mi mercado, selecciona el estilo de tu interes. O habla directamente conmigo
-          </div>
-                  <div className='fondo-comercio'>
-
-                  </div>
-          </div>        
-      }
+     {Id === 5 &&          
+  <div className= 'container2'> 
+    <div className={`close-button ${isNormal ? 'normal' : 'ampliado'}`} onClick={() => closeModal()}></div>
+    <div className='chat-burbuja'> 
+     {displayText}
+    </div> 
+    <div className={`fondo-comercio ${isNormal ? 'normal' : 'ampliado'}`}>
+      <button  className={`left-arrow-button ${isNormal ? '' : 'rotate'}`} onClick={toggleAnimation}/>
+      <div className={`form-container ${isNormal ? 'normal' : 'ampliado'}`}>
+      <div>
+        <label htmlFor="selector" className={`input-label ${isNormal ? 'normal' : 'ampliado'}`}>Selecciona un tipo de servicio:</label>
+        <select id="selector" className={`select-input ${isNormal ? 'normal' : 'ampliado'}`} value={selectorValue} onChange={(e) => setSelectorValue(e.target.value)}>
+          <option value="" disabled hidden>Selecciona una opción</option>
+          <option value="Codigo De Notificaciones">Código De Notificaciones</option>
+          <option value="Pagina Web">Página Web</option>
+          <option value="Comercio Web">Comercio Web</option>
+          <option value="Red Social">Red Social</option>
+          <option value="Juego">Juego</option>
+          <option value="Otro">Otro</option>
+          <option value="Codigo De Automatizaciones">Código De Automatizaciones</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="texto" className={`input-label ${isNormal ? 'normal' : 'ampliado'}`}>Dame una descripción de lo que buscas:</label>
+        <textarea id="texto" className={`text-input-description ${isNormal ? 'normal' : 'ampliado'}`} value={inputValue} onChange={(e) => setInputValue(e.target.value)}></textarea>
+      </div>
+      <div>
+        <label htmlFor="nombre" className={`input-label ${isNormal ? 'normal' : 'ampliado'}`}>Nombre:</label>
+        <input type="text" id="nombre" className={`text-input ${isNormal ? 'normal' : 'ampliado'}`} value={nombre} onChange={(e) => setNombre(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="email" className={`input-label ${isNormal ? 'normal' : 'ampliado'}`}>Email:</label>
+        <input type="email" id="email" className={`text-input ${isNormal ? 'normal' : 'ampliado'}`} value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="telefono" className={`input-label ${isNormal ? 'normal' : 'ampliado'}`}>Teléfono:</label>
+        <input type="tel" id="telefono" className={`text-input ${isNormal ? 'normal' : 'ampliado'}`} value={telefono} onChange={(e) => setTelefono(e.target.value)} />
+      </div>
+      <button className={`button ${isNormal ? 'normal' : 'ampliado'}`} onClick={ () => handleClick()}>Enviar</button>
+    </div>
+    </div>   
+  </div>     
+}
           {Id === 6  &&
               <p> six</p>
       }
@@ -184,10 +268,16 @@ const PortfolioPage = () => {
               <p> seven</p>
       }
           {Id === 8  &&
-              <p> eight</p>
+              <div>
+              <div className={`container ${imagen}`}> <div className="close-button" onClick={() => closeModal()}></div> </div>
+              <p> ESTO ESTA EN CREACION Y TODAVIA NO FUNCIONA</p>
+              </div>
       }
           {Id === 9  &&
-              <p> nine</p>
+              <div>
+              <div className={`container ${imagen}`}> <div className="close-button" onClick={() => closeModal()}></div> </div>
+              <p> ESTO ESTA EN CREACION Y TODAVIA NO FUNCIONA</p>
+              </div>
       }
     </div>
    </div>   
@@ -199,3 +289,8 @@ const PortfolioPage = () => {
 };
 
 export default PortfolioPage;
+
+
+
+
+//porfolio-415712
