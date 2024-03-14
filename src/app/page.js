2c64e0117fea021421bb94c9bfe1c91d861b78f9
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2024-03-13 23:38:49 -0300

    variaciones en el estilo

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,6 @@ const PortfolioPage = () => {
   const [imagen, setImagen] = useState('Basic');
   const [Id , setId] = useState(0);
   const [dataPosition , setDataPosition] = useState(0);
-  const [ProyectsData , setProyectsData] = useState(0);
   const [gamer , setGamer] = useState(false);
   const [isTransitioning, setIsTransitioning] = useState(false);
   const [showDiv, setShowDiv] = useState(false);
@@ -26,9 +25,9 @@ const PortfolioPage = () => {
     setIsNormal(!isNormal);
   }
 
-
-
   let text = 'Bienvenido a mi mercado, abre la pestaña a tu derecha y rellena la informacion necesaria para que pueda saber que buscas.'
+  
+
   useEffect(() => {
     let currentIndex = 0;
     const intervalId = setInterval(() => {
@@ -38,7 +37,7 @@ const PortfolioPage = () => {
       } else {
         clearInterval(intervalId);
       }
-    }, 50); 
+    }, 25); 
 
     return () => clearInterval(intervalId);
   }, [Id==5]);
@@ -66,7 +65,7 @@ const PortfolioPage = () => {
     "",
     "https://w0.peakpx.com/wallpaper/701/1001/HD-wallpaper-among-us-minimalist-black-background-among-us.jpg",
     "https://i.pinimg.com/564x/ec/44/64/ec44647bcbe23e5f13cc605bc2e57d79.jpg",
-    "https://i.pinimg.com/originals/d1/02/d6/d102d6c1f5c4997c7b268b3eda173e86.gif",
+    "https://i.pinimg.com/originals/c4/0d/b6/c40db678aa354ba91257f6258a1521e1.gif",
    
   ]
 
@@ -157,7 +156,16 @@ const PortfolioPage = () => {
       <div className={`five ${imagen}`} onClick={() =>setId(5)}>  <div className="overlay">Tienda de Software</div></div>
       <div className={`nine ${imagen}`} onClick={() =>setId(9)}>  <div className="overlay">Proyectos</div></div>
       <div className={`seven ${imagen}`} onClick={() =>descargarCurriculum()}>  <div className="overlay">Curriculum</div></div>
-      <div className={`eight ${imagen}`}  onClick={() =>setId(8)}>  <div className="overlay">Conocimientos</div></div>
+      <div className={`eight ${imagen}`}  onClick={() =>setId(8)}>  <div className='video'>
+      <iframe 
+        width="100%" 
+        height="100%" 
+        src={`${imagen == 'Basic' ? "https://www.youtube.com/embed/-KH6ZSavJ6Y" : "https://www.youtube.com/embed/y2ECgOhoDGs"}`} 
+        title="YouTube video player" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowFullScreen
+      ></iframe>
+    </div> </div>
       <div className={`ten ${imagen}`} onClick={() =>cambiarImagen()}>  <div className="overlay">Modificar estilo</div></div>
      
      </div>   </div >
@@ -211,9 +219,9 @@ const PortfolioPage = () => {
         </div>
       }
      {Id === 5 &&          
-  <div className= 'container2'> 
+  <div className= {`container2 ${imagen}`}> 
     <div className={`close-button ${isNormal ? 'normal' : 'ampliado'}`} onClick={() => closeModal()}></div>
-    <div className='chat-burbuja'> 
+    <div className={`chat-burbuja ${imagen}`} onClick={() => handleBurbuja()}> 
      {displayText}
     </div> 
     <div className={`fondo-comercio ${isNormal ? 'normal' : 'ampliado'}`}>
@@ -253,12 +261,6 @@ const PortfolioPage = () => {
     </div>   
   </div>     
 }
-          {Id === 8  &&
-              <div>
-              <div className={`container ${imagen}`}> <div className="close-button" onClick={() => closeModal()}></div> </div>
-              <p> ESTO ESTA EN CREACION Y TODAVIA NO FUNCIONA</p>
-              </div>
-      }
           {Id === 9  &&
             <div className={`container-9 ${imagen}`}>
   <div className="close-button-one" onClick={() => closeModal()}></div>
