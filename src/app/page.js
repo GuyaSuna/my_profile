4b54193a7bf0b6b5152a4f6ed2cf2f038acc9362
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2024-06-30 21:59:47 -0300

    mejora descarga curriculum

--- src/app/page.js
+++ src/app/page.js
@@ -100,13 +100,24 @@ const PortfolioPage = () => {
 
   const descargarCurriculum = () => {
     const url = "/CurriculumNahuelPages.pdf";
-    const link = document.createElement("a");
-    link.href = url;
-    link.setAttribute("download", "CurriculumNahuelPages.pdf");
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error al descargar el archivo');
+        }
+        return response.blob();
+      })
+      .then(blob => {
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.setAttribute('download', 'CurriculumNahuelPages.pdf');
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      })
+      .catch(error => console.error('Error al descargar el archivo:', error));
   };
+  
 
   const handleKeyDown = (event, callback) => {
     if (event.key === "Enter") {
